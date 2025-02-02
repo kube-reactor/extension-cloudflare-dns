ab590fb525726dc5a0ac0ae51225f8dc67ff74da
Adrian Webb <adrian@webb.sh>
2025-02-02 00:32:53 -0500

    Adding Cloudflare environment variables to the reactor initialize script.

--- reactor/initialize.sh
+++ reactor/initialize.sh
@@ -9,3 +9,14 @@ export __cloudflare_dns_extension_dir="${2}"
 export __cloudflare_dns_terraform_dir="${__cloudflare_dns_extension_dir}/terraform"
 
 export __cloudflare_dns_project_dir="${__cloudflare_dns_terraform_dir}/dns"
+#
+# Cloudflare
+#
+export CLOUDFLARE_API_TOKEN="${CLOUDFLARE_API_TOKEN:-}"
+export CLOUDFLARE_DOMAIN="${CLOUDFLARE_DOMAIN:-}"
+
+if [[ ! "$CLOUDFLARE_API_TOKEN" ]] && [[ ! "$CLOUDFLARE_DOMAIN" ]]; then
+  emergency "To provision Cloudflare resources, you must specify CLOUDFLARE_API_TOKEN and CLOUDFLARE_DOMAIN environment variables"
+fi
+
+export TF_VAR_root_domain="$CLOUDFLARE_DOMAIN"
